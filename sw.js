914ef6d3eb308a7981c6d4261a15a84de5d950db
Bukarr <sahdheeq1001@gmail.com>
2025-08-27 20:52:15 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Name of the cache
+const CACHE_NAME = "CampusNote-C1";
+
+// Files to cache
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/CampusNote.html",
+  "/manifest.json",
+  "/sw.js",
+  "/icons/campusnote-icon.png",
+  "/icons/campusnote-icon.png",
+  "/styles.css",
+];
+
+// Install service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate service worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch files
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+// Listen for messages from the main thread
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+}); 
